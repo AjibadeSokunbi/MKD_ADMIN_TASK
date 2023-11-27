2b5e8b9759cd827bc67990045b7df155a4483df3
Ajibade <ajibadesokunbi98@gmail.com>
2023-11-27 20:56:31 +0100

    HEADER

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SubHeader = () => {
-  // Function to format the date
+
   const formatDate = (date) => {
     const day = date.getDate();
     const monthNames = [
